9db18724d3704f2423d483dba07f55fefbebac03
Taliesin Sisson <taliesins@interxion.com>
2015-12-02 14:01:32 +0000

    Can specify the AutoLoginCount

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,14 +1,20 @@
 WINLOGON_KEY = 'HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon'
 
 if node['windows_autologin']['enable']
-  registry_key "set autologon for #{node['windows_autologin']['username']}" do
-    key WINLOGON_KEY
-    values [
+  winlogin_keys = [
       { name: 'AutoAdminLogon', type: :string, data: '1' },
       { name: 'DefaultUsername', type: :string, data: node['windows_autologin']['username'] },
       { name: 'DefaultPassword', type: :string, data: node['windows_autologin']['password'] },
       { name: 'DefaultDomainName', type: :string, data: node['windows_autologin']['domain'] }
     ]
+
+  if node['windows_autologin']['autologoncount']
+    winlogin_keys << { name: 'AutoLogonCount', type: :string, dword: node['windows_autologin']['autologoncount'] }
+  end
+
+  registry_key "set autologon for #{node['windows_autologin']['username']}" do
+    key WINLOGON_KEY
+    values winlogin_keys
     action :create
   end
 else
@@ -23,4 +29,10 @@ else
     values [{ name: 'DefaultPassword', type: :string, data: nil }]
     action :delete
   end
+
+  registry_key 'delete AutoLogonCount' do
+    key WINLOGON_KEY
+    values [{ name: 'AutoLogonCount', type: :dword, data: '0' }]
+    action :delete
+  end  
 end
