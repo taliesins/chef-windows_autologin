904e4ab499d5c96e864010fdb282349744da6f10
Dennis Hoer <dennis.hoer@pearson.com>
2017-02-03 14:50:50 -0700

    Fix serverspec offense

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -7,7 +7,7 @@ describe 'windows_autologin::default' do
       it { should have_property_value('AutoAdminLogon', :type_string, '1') }
       it { should have_property('DefaultUsername', :type_string) }
       it { should have_property('DefaultPassword', :type_string) }
-      it { should_not have_property('DefaultDomainName', :type_string) }
+      it { should have_property('DefaultDomainName', :type_string) }
       it { should_not have_property('AutoLogonCount', :type_dword) }
     end
   end
